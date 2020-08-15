75e656f64812b5a374adf211de5e988e165599f6
Alex Calia <alex.m.calia@outlook.com>
2020-08-15 12:37:44 -0400

    initial branch commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component () {
+  render(){
+    return (
+      <div className="App">
+    
+      </div>
+    );
+  }
 }
 
 export default App;
+
+//Landing header with a Get Started button, or arrow, moves to user input
+
+// User inputs for To and From
+
+// Error handling for empty inputs, regex to prevent special characters from being typed, error when address can't be found
+
+// Users submit map requests
+
+// Get travel time and distance.  Convert time to minutes and store in state
+
+// Genre dropdown or user search for podcast, user submits
+
+// take genre/search value, add time state to API query, returns 10 results
+
+// map results to page, clicking on a podcast will show whether they should walk or bike, under the grid of the results
+
+
+/* Components:
+
+App
+
+Location Input: onChange: handleLocation
+
+Podcast Input: onChange:handlepodcast
+
+Podcast Grid
+
+Travel Mode Result
+
+Reset Button
+
+
+
+*/
+
