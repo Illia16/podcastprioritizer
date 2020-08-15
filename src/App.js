7d78bc44bc6a7e64f8068b16a026cb21412659d6
Illia <illia2143@gmail.com>
2020-08-15 14:03:32 -0400

    initial start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import LocationInput from './LocationsInput';
 import './App.css';
 
-class App extends Component () {
+class App extends Component {
   render(){
     return (
       <div className="App">
-    
+        
+        <LocationInput />
       </div>
     );
   }
