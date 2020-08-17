82905d242dba681dd270842a401af3e7d62fddc7
Racheljjn <jian1160@mylaurier.ca>
2020-08-17 13:15:23 -0400

    Updates

---
+++ precache-manifest.73de2ee07f2994394ce8f25234e6fcc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac989e52dc4e2cb95f3d5086a1d29e1f",
+    "url": "/PodcastPrioritizer.podcastprioritizer/index.html"
+  },
+  {
+    "revision": "9bef5f6d58fa3259c817",
+    "url": "/PodcastPrioritizer.podcastprioritizer/static/css/main.5b71ea41.chunk.css"
+  },
+  {
+    "revision": "8f19934722f0a651432e",
+    "url": "/PodcastPrioritizer.podcastprioritizer/static/js/2.47513f58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PodcastPrioritizer.podcastprioritizer/static/js/2.47513f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bef5f6d58fa3259c817",
+    "url": "/PodcastPrioritizer.podcastprioritizer/static/js/main.821ed783.chunk.js"
+  },
+  {
+    "revision": "3325b3ec3e2387700101",
+    "url": "/PodcastPrioritizer.podcastprioritizer/static/js/runtime-main.f083d92b.js"
+  }
+]);
