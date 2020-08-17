7adc2f5d2c2d4dafe7021a525c1c5f1a177f60fa
Illia <illia2143@gmail.com>
2020-08-16 22:54:29 -0400

    drop-down done, grabbing the text from select-option

--- src/PodcastItem.js
+++ src/PodcastItem.js
@@ -11,62 +11,59 @@ class PodcastItem extends Component {
     displaySuggestion = (e) => {
         e.preventDefault();
         this.setState({
-            displaySuggestion: true,
+            displaySuggestion: !this.state.displaySuggestion,
         });
     };
 
     render(){
-
-        const {transitTime, title, description, length, image} = this.props
-
+        const {transitTime, title, description, length, image} = this.props;
         return(   
-                        <li>
-                                    <button onClick={this.displaySuggestion}>
-                                        <div className="thumbnailWrapper">
-                                            <img
-                                                src={image}
-                                                alt={title}
-                                            ></img>
-                                            <p>{title}</p>
-                                        </div>
-                                    </button>
+                <li>
+                    <button onClick={this.displaySuggestion} className="podcastButton">
+                        <div className="thumbnailWrapper">
+                            <img
+                                src={image}
+                                alt={title}
+                            ></img>
+                            <p>{title}</p>
+                        </div>
+                    </button>
 
-                                    <div
-                                        className="suggestion"
-                                        style={{
-                                            display: this.state.displaySuggestion ? "block" : "none",
-                                        }}
-                                    >
-                                        {
-                                            Math.round(length / 60) <= 1 ? (
-                                                <p>
-                                                    podcast length:{Math.round(length / 60)}{" "}
-                                        minute
-                                                </p>
-                                            ) : (
-                                                    <p>
-                                                        podcast length:{Math.round(length / 60)}{" "}
-                                        minutes
-                                                    </p>
-                                                )
-                                        }
+                    <div 
+                        onClick={this.displaySuggestion}
+                        className="suggestion"
+                        style={{
+                            display: this.state.displaySuggestion ? "block" : "none",
+                        }}
+                    >
+                        {
+                            Math.round(length / 60) <= 1 ? (
+                                <p>
+                                    podcast length:{Math.round(length / 60)}{" "}
+                        minute
+                                </p>
+                            ) : (
+                                    <p>
+                                        podcast length:{Math.round(length / 60)}{" "}
+                        minutes
+                                    </p>
+                                )
+                        }
 
-                                        <p> walk:{transitTime.pedestrian} minutes</p>
-                                        <p>bike:{transitTime.bicycle} minutes</p>
-                                        <p>drive:{transitTime.fastest} minutes</p>
+                        <p> walk:{transitTime.pedestrian} minutes</p>
+                        <p>bike:{transitTime.bicycle} minutes</p>
+                        <p>drive:{transitTime.fastest} minutes</p>
 
-                                        {
-                                            length / 60 < transitTime.fastest ?
-                                                <p>suggestion: you should drive</p> :
-                                                length / 60 < transitTime.bicycle ?
-                                                    <p>suggestion: you should bike</p> :
-                                                    <p>suggestion: you should walk</p>
-                                        }
-                                    </div>
-                                </li>
-                    )
-            
-        
+                        {
+                            length / 60 < transitTime.fastest ?
+                                <p>suggestion: you should drive</p> :
+                                length / 60 < transitTime.bicycle ?
+                                    <p>suggestion: you should bike</p> :
+                                    <p>suggestion: you should walk</p>
+                        }
+                    </div>
+                </li>
+                )
     }
 }
 
