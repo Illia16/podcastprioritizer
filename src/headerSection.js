964399acca7a30790263e25520be8db75bf351f4
Sierra MacDonald <sierrainertia@gmail.com>
2020-08-19 14:07:37 -0400

    Menu style changes, header changes, form changes

--- src/headerSection.js
+++ src/headerSection.js
@@ -2,8 +2,10 @@ import React from "react"
 
 const headerSection = () => {
     return (<header>
-        <h1>Podcast Prioritizer <i className="fas fa-headphones" aria-label="Icon of headphones"></i></h1>
-        <p>Can't decide which podcast to listen to on your next journey? Not sure whether you should walk, bike or drive? Use this web app by inputting your 'Starting Point', 'Destination', and a podcast search term to determine which podcast you should listen to, and how you should get there.</p>
+        <h1><i className="fas fa-headphones" aria-label="Icon of headphones"></i> Podcast Prioritizer</h1>
+        <p>Can't decide which podcast to listen to on your next journey? Not sure whether you should walk, bike or drive? Input your <span>Starting Point</span>, <span>Destination</span>, and a <span>Podcast Search</span> term to determine which podcast you should listen to, and how you should get there.</p>
+
+        <p>To save a podcast for later, log in using you google account by using the menu button in the top right. Once logged in you can save podcasts for later using the 'Save Podcast' button.</p>
     </header>
     )
 }
