2847b0e66a09e111820b02904d5f1b49face0a77
Sierra MacDonald <sierrainertia@gmail.com>
2020-08-19 23:18:50 -0400

    making responsive & style changes :art: :sweat:

--- src/PodcastItem.js
+++ src/PodcastItem.js
@@ -2,56 +2,56 @@ import React, { Component } from 'react';
 import Suggestion from "./Suggestion";
 
 class PodcastItem extends Component {
-    constructor() {
-        super();
-        this.state = {
-            displaySuggestion: false
-        }
+  constructor() {
+    super();
+    this.state = {
+      displaySuggestion: false
     }
+  }
 
-    showHideSuggestion = () => {
-        this.setState({
-            displaySuggestion: !this.state.displaySuggestion,
-        });
-    };
+  showHideSuggestion = () => {
+    this.setState({
+      displaySuggestion: !this.state.displaySuggestion,
+    });
+  };
 
-    render() {
+  render() {
 
-        const { title, image, savePodcast, audio, id, loggedIn, url,length,transitTime } = this.props;
-        return (
-          <li
-            onMouseEnter={this.showHideSuggestion}
-            onMouseLeave={this.showHideSuggestion}
-            className="podcastContainer"
-          >
-            {/* <div className="podcastContainer"> */}
-            <div className="podcastPiece">
-              <img src={image} alt={title}></img>
-            </div>
-              <a href={url} target="_blank">listen here</a>
+    const { title, image, savePodcast, audio, id, loggedIn, url, length, transitTime } = this.props;
+    return (
+      <li
+        onMouseEnter={this.showHideSuggestion}
+        onMouseLeave={this.showHideSuggestion}
+        className="podcastContainer"
+      >
+        {/* <div className="podcastContainer"> */}
+        <div className="podcastPiece">
+          <img src={image} alt={title}></img>
+        </div>
+        <a href={url} target="_blank">Listen Here</a>
 
-            <div>
-              {this.state.displaySuggestion ? (
-                <Suggestion length={length} transitTime={transitTime} />
-              ) : (
-                <p>{title}</p>
-              )}
-            </div>
+        <div>
+          {this.state.displaySuggestion ? (
+            <Suggestion length={length} transitTime={transitTime} />
+          ) : (
+              <p>{title}</p>
+            )}
+        </div>
 
-            <div className="podcastSave">
-              {loggedIn && (
-                <button
-                  onClick={(e) => {
-                    savePodcast(e, title, image, audio, id);
-                  }}
-                >
-                  Save Podcast
-                </button>
-              )}
-            </div>
-          </li>
-        );
-    }
+        <div className="podcastSave">
+          {loggedIn && (
+            <button
+              onClick={(e) => {
+                savePodcast(e, title, image, audio, id);
+              }}
+            >
+              Save Podcast
+            </button>
+          )}
+        </div>
+      </li>
+    );
+  }
 }
 
 export default PodcastItem;
--- src/headerSection.js
+++ src/headerSection.js
@@ -3,9 +3,9 @@ import React from "react"
 const headerSection = () => {
     return (<header>
         <h1><i className="fas fa-headphones" aria-label="Icon of headphones"></i> Podcast Prioritizer</h1>
-        <p>Can't decide which podcast to listen to on your next journey? Not sure whether you should walk, bike or drive? Input your <span>Starting Point</span>, <span>Destination</span>, and a <span>Podcast Search</span> term to determine which podcast you should listen to, and how you should get there.</p>
+        <p>Can't decide which podcast to listen to on your next journey? Not sure whether you should walk, bike or drive? Input your <span role="text">Starting Point</span>, <span role="text">Destination</span>, and a <span role="text">Podcast Search</span> term to determine which podcast you should listen to, and how you should get there.</p>
 
-        <p>To save a podcast for later, log in using you google account by using the menu button in the top right. Once logged in you can save podcasts for later using the 'Save Podcast' button.</p>
+        <p>To save a podcast for later, log in using your google account by using the menu button in the top right. Once logged in you can save podcasts for later using the 'Save Podcast' button.</p>
     </header>
     )
 }
